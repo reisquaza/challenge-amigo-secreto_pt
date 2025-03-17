1ae580f8d9c29a60b4ebd1710b109309929bdc9a
Joao Victor Reis <joaovictor.reis2000@gmail.com>
2025-03-16 23:10:15 -0300

    feat: update friendlist

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const amigos = [];
 const inputAmigo = document.getElementById("amigo")
 
 function adicionarAmigo() {
-    console.log("inputamigo", inputAmigo.value);
     const inputValue = inputAmigo.value;
     if (inputValue === "") {
         alert("Por favor, insira um nome.")
     }
     amigos.push(inputValue);
     inputAmigo.value = "";
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        const amigo = amigos[i];
+        const elemento = document.createElement("li")
+        elemento.innerText = amigo;
+        lista.appendChild(elemento);
+    }
 }
