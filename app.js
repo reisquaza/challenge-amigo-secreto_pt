3eb752cb376a69b94a7d428204ce6bdb26b33c44
Joao Victor Reis <joaovictor.reis2000@gmail.com>
2025-03-16 23:20:23 -0300

    feat: draw a friend

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const amigos = [];
-const inputAmigo = document.getElementById("amigo")
+const inputAmigo = document.getElementById("amigo");
 
 function adicionarAmigo() {
     const inputValue = inputAmigo.value;
     if (inputValue === "") {
         alert("Por favor, insira um nome.")
-    }
+    };
     amigos.push(inputValue);
     inputAmigo.value = "";
     atualizarListaAmigos();
@@ -20,4 +20,19 @@ function atualizarListaAmigos() {
         elemento.innerText = amigo;
         lista.appendChild(elemento);
     }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("A lista de amigos está vázia.");
+        return;
+    }
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length) 
+    console.log("first", indiceAleatorio);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const elemento = document.createElement("li");
+    elemento.innerText = `O amigo sorteado foi: ${amigoSorteado}`;
+    resultado.appendChild(elemento);
 }
