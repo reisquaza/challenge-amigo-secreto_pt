5c1254098102377f55173c39437295fd9df92b85
Joao Victor Reis <joaovictor.reis2000@gmail.com>
2025-03-16 23:03:36 -0300

    feat: add friend

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const amigos = [];
+const inputAmigo = document.getElementById("amigo")
+
+function adicionarAmigo() {
+    console.log("inputamigo", inputAmigo.value);
+    const inputValue = inputAmigo.value;
+    if (inputValue === "") {
+        alert("Por favor, insira um nome.")
+    }
+    amigos.push(inputValue);
+    inputAmigo.value = "";
+}
